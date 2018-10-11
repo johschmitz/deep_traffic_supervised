7ff03447140ba60c2a75ec9745cbb520366b740f
Johannes Schmitz <johannes.schmitz1@gmail.com>
2018-10-11 16:16:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# DeepTraffic (semi-)supervised
+
+## Basics
+This is in fact a kind of semi-supervised model for the MIT DeepTraffic excercise.
+The user is able to control the car with the `arrow` keys.
+Additional keys are `s` to activate the supervised mode and `l` to activate the interactive learning process.
+The car can learn in the supervised as well as in the reinforcement learning mode.
+
+## Idea
+
+When supervision is active the car is rewarded for following the actions of the user.
+On the other hand, when supervision is turned off, the Q-learning approach is followed as implemented in the baseline DeepTraffic.
+One idea is to take over whenever the car gets stuck in a difficult situation.
+Another idea is that it will be able to learn faster in the beginning when it is totally untrained.
+
+## How to run
+Download the `semi_supervised_model.js` file and load it into the [MIT DeepTraffic](https://selfdrivingcars.mit.edu/deeptraffic/) website.
+It is possible to set the simulation to `fast` mode.
+
+### Note
+Tested with version 2.0.
+
+
---
+++ semi_supervised_model.js
@@ -0,0 +1,192 @@
+
+//<![CDATA[
+
+// a few things don't have var in front of them - they update already existing variables the game needs
+lanesSide = 5;
+patchesAhead = 50;
+patchesBehind = 10;
+trainIterations = 1000000;
+
+// the number of other autonomous vehicles controlled by your network
+otherAgents = 0; // max of 10
+
+var num_inputs = (lanesSide * 2 + 1) * (patchesAhead + patchesBehind);
+var num_actions = 5;
+var temporal_window = 0;
+var network_size = num_inputs * temporal_window + num_actions * temporal_window + num_inputs;
+
+var layer_defs = [];
+    layer_defs.push({
+    type: 'input',
+    out_sx: 1,
+    out_sy: 1,
+    out_depth: network_size
+});
+layer_defs.push({
+   type: 'fc',
+   num_neurons: 40,
+   activation: 'relu'
+});
+layer_defs.push({
+   type: 'fc',
+   num_neurons: 40,
+   activation: 'relu'
+});
+layer_defs.push({
+   type: 'fc',
+   num_neurons: 40,
+   activation: 'relu'
+});
+layer_defs.push({
+   type: 'fc',
+   num_neurons: 40,
+   activation: 'relu'
+});
+layer_defs.push({
+   type: 'fc',
+   num_neurons: 15,
+   activation: 'relu'
+});
+layer_defs.push({
+    type: 'regression',
+    num_neurons: num_actions
+});
+
+var tdtrainer_options = {
+    learning_rate: 0.001,
+    momentum: 0.0,
+    batch_size: 32,
+    l2_decay: 0.01
+};
+
+var opt = {};
+//opt.random_action_distribution=[0.2,0.15,0.05,0.3,0.3]
+opt.temporal_window = temporal_window;
+opt.experience_size = 30000;
+opt.start_learn_threshold = 5000;
+opt.gamma = 0.9;
+opt.learning_steps_total = 10000;
+opt.learning_steps_burnin = 1000;
+opt.epsilon_min = 0.1;
+opt.epsilon_test_time = 0.0;
+opt.layer_defs = layer_defs;
+opt.tdtrainer_options = tdtrainer_options;
+
+brain = new deepqlearn.Brain(num_inputs, num_actions, opt);
+
+
+// we require some addition global variables
+supervision = false;
+// check for document to avoid crashing in webworker
+if (typeof self.document != 'undefined') {
+    speedBox = document.getElementsByClassName('speedBox')[0];
+    speedBox.innerHTML = 'Speed:<br><span style="font-size:16pt;" id="mph">0</span> mph<br>\
+                       Cars Passed: <br><span style="font-size:16pt;" id="passed">0</span><br>\
+                       Supervision<br>(activate with<br>"s" key, arrow keys<br> for steering):<br><span id="supervision" style="font-size:16pt">Off</span><br>\
+                       Learning<br>(activate with<br>"l" key):<br><span id="learning" style="font-size:16pt">Off</span>';
+    span_supervision = document.getElementById('supervision');
+    span_learning = document.getElementById('learning');
+}
+
+pressed = {};
+window.onkeydown = function(event){
+    event = event || window.event;
+    pressed[event.keyCode] = true;
+    //console.log(event.keyCode);
+}
+
+print_action = function(action) {
+    switch (action) {
+        case 0:
+            console.log('No action');
+            break;
+        case 1:
+            console.log('Accelerate');
+            break;
+        case 2:
+            console.log('Decelerate');
+            break;
+        case 3:
+            console.log('Go left');
+            break;
+        case 4:
+            console.log('Go right');
+            break;
+        default:
+            console.log('Error: Wrong action, must be in {0,..,4}.');
+    }
+}
+
+learn = function (state, lastReward) {
+    var action_net = 0;
+    var action_supervisor = 0;
+    var action = 0;
+
+    if (pressed["83"]) {
+        // s key activates supervision
+        if (supervision == false) {
+            supervision = true;
+            // enable interactive learning
+            span_supervision.innerHTML = 'On';
+        }
+        else {
+            supervision = false;
+            // disable interactive learning
+            span_supervision.innerHTML = 'Off';
+        }
+    } else if (pressed["76"]) {
+        // l key activates learning
+        if (brain.learning == false) {
+            brain.learning = true;
+            span_learning.innerHTML = 'On';
+        } else {
+            brain.learning = false;
+            span_learning.innerHTML = 'Off';            
+        }
+        console.log('brain.learning: '+brain.learning)
+    } else if (pressed["37"]) {
+        // left arrow, go left
+        action_supervisor = 3;
+    } else if (pressed["38"]) {
+        // up arrow, accelerate
+        action_supervisor = 1;
+    } else if (pressed["39"]) {
+        // right arrow, go right
+        action_supervisor = 4;
+    } else if (pressed["40"]) {
+        // down arrow, decelerate
+        action_supervisor = 2;
+    } 
+    // reset button states
+    pressed = {};
+
+    if (supervision == true) {
+        // train based on the supervisors actions
+        //console.log('Supervisor action: ');
+        //print_action(action_supervisor)
+        action_net = brain.forward(state);
+        var reward;
+        if (action_supervisor == action_net) {
+            reward = 3;
+        } else {
+            reward = 0;
+        }
+        brain.backward(reward);
+        action = action_supervisor;
+    } else {
+        // ignore supervision and use reinforcement learning
+        brain.backward(lastReward);
+        action_net = brain.forward(state);
+        action = action_net;
+    }
+
+    //console.log('Brain action: ');
+    //print_action(action_net);
+
+    draw_net();
+    draw_stats();
+
+    return action;
+}
+
+//]]>
